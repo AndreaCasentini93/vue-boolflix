2e26e1b4958557062116e2ca25cab2a3f5ceb501
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-14 11:27:04 +0200

    Axios Call

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,38 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     
+  },
+  data: function() {
+    return {
+      apiUrl: 'https://api.themoviedb.org/3/search/movie',
+      title: 'ritorno al futuro'
+    }
+  },
+  created: function() {
+    axios
+      .get(this.apiUrl, {
+        params: {
+          api_key: 'cb304e29663a7b9973c26a03b4532795',
+          language: 'it-IT',
+          query: this.title
+        }
+      })
+      .then (response => {
+        console.log(response.data.results);
+      })
+      .catch()
   }
 }
 </script>
 
 <style lang="scss">
-  
+  @import './assets/style/general.scss';
+  @import './assets/style/mixins.scss';
+  @import './assets/style/variables.scss';
 </style>
