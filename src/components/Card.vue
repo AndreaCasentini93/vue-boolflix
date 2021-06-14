2bac775644902866bb1fd29771b61039696cf942
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-14 14:19:02 +0200

    Flag

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="text-debug">Card</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/style/variables.scss';
+    @import '../assets/style/mixins.scss';
+
+    .text-debug {
+        color: orange;
+    }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,25 +1,37 @@
 <template>
     <div class="container">
+        <Card />
         <ul>
             <li v-for="movie, index in movies" :key="index">
-                {{ movie.title }}<br>
-                {{ movie.original_title }}<br>
-                {{ movie.original_language }}<br>
-                {{ (movie.vote_average * 5) / 10 }}
+                <div>{{ movie.title }}</div>
+                <div>{{ movie.original_title }}</div>
+                <img v-if="movie.original_language == 'en' || movie.original_language == 'it'" src="" alt="">
+                <div v-else>{{ movie.original_language }}</div>
+                <div>{{ (movie.vote_average * 5) / 10 }}</div>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
+import Card from './Card.vue'
+
 export default {
     name: 'Main',
+    components: {
+        Card
+    },
     props: {
         searchArray: Array
     },
     computed: {
         movies: function() {
             return this.searchArray;
+        }
+    },
+    methods: {
+        languageFlag: function(language) {
+            language == 'en' || language == 'it';
         }
     }
 }
